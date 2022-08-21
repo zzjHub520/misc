5afaf7e4a452d3ffb2bb56aa04e51f161e71d82e
zzj <912290893@qq.com>
2022-08-22 00:25:26 +0800

    asdf

--- filebeat/auto-filebeat.sh
+++ filebeat/auto-filebeat.sh
@@ -7,8 +7,11 @@ startupScriptName=aahhh
 cat > ${basepath}/${startupScriptName}.sh <<EOF
 #!/bin/bash
 
-nohup java -jar ${basepath}/autostart-test-0.0.1-SNAPSHOT.jar > ${basepath}/log.log 2>&1 &
+if [ `ps -ef | grep -v grep | grep autostart-test-0.0.1-SNAPSHOT.jar | wc -l` -eq 0 ] ; then
+	nohup java -jar ${basepath}/autostart-test-0.0.1-SNAPSHOT.jar > ${basepath}/log.log 2>&1 &
+fi
 
 EOF
 
 mv ${basepath}/${startupScriptName}.sh /etc/profile.d/
+
